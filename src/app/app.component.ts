ce8b445b8cbe352ba743d85335bf2f6053c6eb1f
ch.domino <chrobakdomino@gmail.com>
2024-02-21 10:15:04 +0100

    Add Angular Material card module and update login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsersComponent],
+  imports: [RouterOutlet, UsersComponent, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
+/**
+ * @title Card with multiple sections
+ */
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class LoginComponent {
-
+  hide = true;
 }
+export class CardFancyExample {}
