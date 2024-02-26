1495290809507fd247a257f6345ec51377c18bbb
ch.domino <chrobakdomino@gmail.com>
2024-02-26 13:38:17 +0100

    Add extended users component to routes

---
+++ src/app/extended-users/extended-users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extended-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './extended-users.component.html',
+  styleUrl: './extended-users.component.css'
+})
+export class ExtendedUsersComponent {
+
+}
