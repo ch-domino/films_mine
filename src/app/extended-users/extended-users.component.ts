a73c0100177ad3174239a453e37a4fd334bceb5e
ch.domino <chrobakdomino@gmail.com>
2024-02-28 07:53:42 +0100

    Remove unnecessary import in extended-users.component.ts

--- src/app/extended-users/extended-users.component.ts
+++ src/app/extended-users/extended-users.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit, inject } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { User } from '../../entities/user';
 import { MaterialModule } from '../../modules/material.module';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-extended-users',
   standalone: true,
-  imports: [MaterialModule, RouterLink],
+  imports: [MaterialModule],
   templateUrl: './extended-users.component.html',
   styleUrl: './extended-users.component.css',
 })
