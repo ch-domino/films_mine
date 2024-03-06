0f4db1b01d62bdce0e9e339cb28ac76ffa422a90
ch.domino <chrobakdomino@gmail.com>
2024-03-06 09:53:18 +0100

    Add inject function to ConfirmService

--- src/services/confirm.service.ts
+++ src/services/confirm.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import {
+  ConfirmDialogComponent,
+  ConfirmDialogData,
+} from '../app/confirm-dialog/confirm-dialog.component';
+import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConfirmService {
+  dialog = inject(MatDialog);
 
-  constructor() { }
+  confirm(data: ConfirmDialogData): Observable<boolean> {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, { data });
+
+    return dialogRef.afterClosed();
+  }
 }
