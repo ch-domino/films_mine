c1894d2f5c227f4b521541ea620a5af638be034b
ch.domino <chrobakdomino@gmail.com>
2024-03-11 11:10:55 +0100

    Update user editing functionality

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-user',
   standalone: true,
   imports: [],
   templateUrl: './edit-user.component.html',
-  styleUrl: './edit-user.component.css'
+  styleUrl: './edit-user.component.css',
 })
-export class EditUserComponent {
+export class EditUserComponent implements OnInit {
+  route = inject(ActivatedRoute);
+  userId?: string;
 
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.params['id'];
+  }
 }
