a364a02708401c938e8c7cf448043b6b05888143
ch.domino <chrobakdomino@gmail.com>
2024-03-11 11:21:16 +0100

    Add styling and navigation buttons to edit-user component

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { MaterialModule } from '../../modules/material.module';
 
 @Component({
   selector: 'app-edit-user',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, RouterLink],
   templateUrl: './edit-user.component.html',
   styleUrl: './edit-user.component.css',
 })
