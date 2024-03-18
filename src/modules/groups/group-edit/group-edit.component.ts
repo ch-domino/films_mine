ba44e5d1240b49b2c58a7399166b16821562de1a
ch.domino <chrobakdomino@gmail.com>
2024-03-18 13:13:11 +0100

    Update GroupEditComponent with CanDeactivateGuard and ConfirmService

--- src/modules/groups/group-edit/group-edit.component.ts
+++ src/modules/groups/group-edit/group-edit.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { GroupEditChildComponent } from '../group-edit-child/group-edit-child.component';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map, of, switchMap } from 'rxjs';
+import { Observable, map, of, switchMap } from 'rxjs';
 import { UsersService } from '../../../services/users.service';
 import { Group } from '../../../entities/group';
+import { CanDeactivateComponent } from '../../../guards/can-deactivate.guard';
+import { ConfirmService } from '../../../services/confirm.service';
 
 @Component({
   selector: 'app-group-edit',
@@ -12,11 +14,13 @@ import { Group } from '../../../entities/group';
   templateUrl: './group-edit.component.html',
   styleUrl: './group-edit.component.css',
 })
-export class GroupEditComponent implements OnInit {
+export class GroupEditComponent implements OnInit, CanDeactivateComponent {
   usersService = inject(UsersService);
+  confirmService = inject(ConfirmService);
   router = inject(Router);
   route = inject(ActivatedRoute);
   group: Group = new Group('');
+  saved = false;
 
   ngOnInit(): void {
     this.route.paramMap
@@ -32,5 +36,16 @@ export class GroupEditComponent implements OnInit {
 
   groupSaved(group: Group) {
     this.router.navigateByUrl('/groups');
+    this.saved = true;
+  }
+
+  canDeactivate(): boolean | Observable<boolean> {
+    if (this.saved) {
+      return true;
+    }
+    return this.confirmService.confirm({
+      title: 'Edit not saved',
+      question: 'Group not saved, do you want to leave anyway?',
+    });
   }
 }
