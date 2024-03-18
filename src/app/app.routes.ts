889210902ac486b6755f280faf71d0e708a6961f
ch.domino <chrobakdomino@gmail.com>
2024-03-18 13:38:20 +0100

    Add authMatchGuard to groups route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { ExtendedUsersComponent } from './extended-users/extended-users.component';
 import { RegisterComponent } from './register/register.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
-import { authGuard } from '../guards/auth.guard';
+import { authGuard, authMatchGuard } from '../guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'users', component: UsersComponent },
@@ -21,6 +21,8 @@ export const routes: Routes = [
   {
     path: 'groups',
     loadChildren: () => import('../modules/groups/groups.module'), //.then((mod) => mod.GroupsModule),
+    canActivate: [authGuard],
+    canMatch: [authMatchGuard],
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
