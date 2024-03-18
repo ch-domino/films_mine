e3dad0dab7197809d6420c8151e4c0ea84575ccd
ch.domino <chrobakdomino@gmail.com>
2024-03-18 13:12:59 +0100

    Add resolve-group.guard.ts for resolving group data

---
+++ src/guards/resolve-group.guard.ts
@@ -0,0 +1,14 @@
+import { ResolveFn } from '@angular/router';
+import { Group } from '../entities/group';
+import { EMPTY } from 'rxjs';
+import { inject } from '@angular/core';
+import { UsersService } from '../services/users.service';
+
+export const groupResolver: ResolveFn<Group> = (route, state) => {
+  const Id = Number(route.paramMap.get('id'));
+  if (!Id) {
+    return EMPTY;
+  }
+  const userService = inject(UsersService);
+  return userService.getGroup(Id);
+};
