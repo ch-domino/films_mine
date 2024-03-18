885683f9238e900a11fb8a7ea85012843cd85a8f
ch.domino <chrobakdomino@gmail.com>
2024-03-18 12:17:26 +0100

    Add authentication guard to extended-users route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,15 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { ExtendedUsersComponent } from './extended-users/extended-users.component';
 import { RegisterComponent } from './register/register.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
+import { authGuard } from '../guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'users', component: UsersComponent },
-  { path: 'extended-users', component: ExtendedUsersComponent },
+  {
+    path: 'extended-users',
+    component: ExtendedUsersComponent,
+    canActivate: [authGuard],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'user/edit/:id', component: EditUserComponent },
