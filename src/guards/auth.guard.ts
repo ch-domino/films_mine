0afab6c3e76adab441c486ef210708bf80131c6d
ch.domino <chrobakdomino@gmail.com>
2024-03-18 12:48:47 +0100

    Add router navigation for unauthorized users in auth guard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,8 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { UsersService } from '../services/users.service';
 import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const usersService = inject(UsersService);
-  return usersService.isLoggedIn();
+  const router = inject(Router);
+  if (usersService.isLoggedIn()) {
+    return true;
+  }
+  usersService.redirectAfterLogin = state.url;
+  router.navigateByUrl('/login');
+  return false;
 };
