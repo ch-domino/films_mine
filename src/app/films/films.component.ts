ac83c8cb7c669439228d86f7466c93aac46e2bb2
ch.domino <chrobakdomino@gmail.com>
2024-03-20 09:51:38 +0100

    Update films component with new imports and functionality

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -3,12 +3,16 @@ import {
   Component,
   computed,
   inject,
+  signal,
   viewChild,
 } from '@angular/core';
-import { Film } from '../../entities/film';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { switchMap, tap } from 'rxjs';
+import { MaterialModule } from '../../modules/material.module';
 import { FilmsService } from '../../services/films.service';
 import { UsersService } from '../../services/users.service';
-import { MaterialModule } from '../../modules/material.module';
 
 @Component({
   selector: 'app-films',
@@ -22,17 +26,72 @@ export class FilmsComponent implements AfterViewInit {
   usersService = inject(UsersService);
   userNameS = this.usersService.loggedUserSignal;
 
-  films: Film[] = [];
   columnsToDisplay = computed(() =>
     this.userNameS()
       ? ['id', 'nazov', 'rok', 'slovenskyNazov', 'afi1998', 'afi2007']
       : ['id', 'nazov', 'rok']
   );
+  paginatorS = viewChild.required<MatPaginator>(MatPaginator);
+  sortS = viewChild.required<MatSort>(MatSort);
+  fiterS = signal('');
+
+  orderByS = signal<string | undefined>(undefined);
+  descendingS = signal<boolean | undefined>(undefined);
+  indexFromS = signal<number | undefined>(undefined);
+  indexToS = signal<number | undefined>(undefined);
+  searchS = signal<string | undefined>(undefined);
+
+  quesryS = computed(
+    () =>
+      new Query(
+        this.orderByS(),
+        this.descendingS(),
+        this.indexFromS(),
+        this.indexToS(),
+        this.searchS()
+      )
+  );
+
+  request$ = toObservable(this.quesryS).pipe(
+    tap((query) => console.log('request', query)),
+    switchMap((query) =>
+      this.filmsService.getFilms(
+        query.orderBy,
+        query.descending,
+        query.indexFrom,
+        query.indexTo,
+        query.search
+      )
+    )
+  );
+  responseS = toSignal(this.request$);
+  filmsS = computed(() => this.responseS()?.items || []);
 
   ngAfterViewInit(): void {
-    this.filmsService.getFilms().subscribe((filmsResponse) => {
-      console.log(filmsResponse);
-      this.films = filmsResponse.items;
+    this.paginatorS().page.subscribe((pageEvent) => {
+      console.log('pageEvent', pageEvent);
+      this.indexFromS.set(pageEvent.pageIndex * pageEvent.pageSize);
+      this.indexToS.set(
+        Math.min(
+          (pageEvent.pageIndex + 1) * pageEvent.pageSize,
+          pageEvent.length
+        )
+      );
     });
   }
+
+  onFilter(event: any) {
+    const filter = (event.target.value as string).trim().toLowerCase();
+    this.fiterS.set(filter);
+  }
+}
+
+class Query {
+  constructor(
+    public orderBy?: string,
+    public descending?: boolean,
+    public indexFrom?: number,
+    public indexTo?: number,
+    public search?: string
+  ) {}
 }
