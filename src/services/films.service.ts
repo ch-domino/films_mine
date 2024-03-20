6cf6c2938543589e7f3b23669bd3331adbbe89b3
ch.domino <chrobakdomino@gmail.com>
2024-03-20 07:58:26 +0100

    Add token authentication to FilmsService

--- src/services/films.service.ts
+++ src/services/films.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Film } from '../entities/film';
 import { UsersService } from './users.service';
+import { environement } from '../app/environments/environment.development';
 
 export interface FilmsResponse {
   items: Film[];
@@ -15,9 +16,23 @@ export interface FilmsResponse {
 export class FilmsService {
   usersService = inject(UsersService);
   http = inject(HttpClient);
-  url = 'http://localhost:8080/';
+  url = environement.serverUrl;
+
+  get token() {
+    return this.usersService.token;
+  }
+
+  getTokenHeader(): { headers: { [header: string]: string } } | undefined {
+    if (!this.token) {
+      return undefined;
+    }
+    return { headers: { 'X-Auth-Token': this.token } };
+  }
 
   getFilms(): Observable<FilmsResponse> {
-    return this.http.get<FilmsResponse>(this.url + 'films');
+    return this.http.get<FilmsResponse>(
+      this.url + 'films',
+      this.getTokenHeader()
+    );
   }
 }
