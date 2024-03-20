9206aa92ccbf8274ae9eb2af3121a651d29744ab
ch.domino <chrobakdomino@gmail.com>
2024-03-20 08:17:21 +0100

    Refactor getFilms method to accept optional parameters

--- src/services/films.service.ts
+++ src/services/films.service.ts
@@ -22,17 +22,51 @@ export class FilmsService {
     return this.usersService.token;
   }
 
-  getTokenHeader(): { headers: { [header: string]: string } } | undefined {
+  getTokenHeader():
+    | { headers?: { [header: string]: string }; params?: HttpParams }
+    | undefined {
     if (!this.token) {
       return undefined;
     }
     return { headers: { 'X-Auth-Token': this.token } };
   }
 
-  getFilms(): Observable<FilmsResponse> {
-    return this.http.get<FilmsResponse>(
-      this.url + 'films',
-      this.getTokenHeader()
-    );
+  getFilms(
+    orderBy?: string,
+    descending?: boolean,
+    indexFrom?: number,
+    indexTo?: number,
+    search?: string
+  ): Observable<FilmsResponse> {
+    let options = this.getTokenHeader();
+    if (orderBy || descending || indexFrom || indexTo || search) {
+      options = {
+        ...(options || {}),
+        params: new HttpParams()
+          .set('orderBy', orderBy || '')
+          .set('descending', descending ? 'true' : 'false')
+          .set('indexFrom', indexFrom?.toString() || '')
+          .set('indexTo', indexTo?.toString() || '')
+          .set('search', search || ''),
+      };
+    }
+    if (options && options.params) {
+      if (orderBy) {
+        options.params.append('orderBy', orderBy);
+      }
+      if (descending) {
+        options.params.append('descending', descending);
+      }
+      if (indexFrom) {
+        options.params.append('indexFrom', indexFrom);
+      }
+      if (indexTo) {
+        options.params.append('indexTo', indexTo);
+      }
+      if (search) {
+        options.params.append('search', search);
+      }
+    }
+    return this.http.get<FilmsResponse>(this.url + 'films', options);
   }
 }
