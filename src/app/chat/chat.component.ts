5a3d99f0a3547b309e00ad82f5ce57c84b0833cd
ch.domino <chrobakdomino@gmail.com>
2024-03-25 13:11:37 +0100

    Add chat functionality to ChatComponent

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { tap } from 'rxjs';
 import { MaterialModule } from '../../modules/material.module';
-import { ChatService } from '../../services/chat.service';
+import { ChatMessage, ChatService } from '../../services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -13,11 +14,44 @@ import { ChatService } from '../../services/chat.service';
 export class ChatComponent {
   name = '';
   chatService = inject(ChatService);
+  messages: ChatMessage[] = [];
+  msgToSend = '';
 
   connect() {
-    this.chatService.connect().subscribe((ok) => {
-      console.log('Connected!');
+    this.chatService
+      .connect()
+      .pipe(
+        tap((ok) => {
+          if (ok) {
+            this.listenToEndpoints();
+            this.chatService.sendName(this.name);
+          }
+        })
+      )
+      .subscribe((ok) => {
+        console.log('Connected!');
+      });
+  }
+
+  listenToEndpoints() {
+    this.chatService.listenToMessages().subscribe((message) => {
+      this.messages = [...this.messages, message];
+    });
+    this.chatService.listenToGreetings().subscribe((message) => {
+      this.messages = [...this.messages, message];
     });
   }
-  disconnect() {}
+
+  sendMessage() {
+    this.chatService.sendMessage(this.msgToSend);
+  }
+
+  disconnect() {
+    console.log('Disconnecting...');
+    this.chatService.disconnect();
+  }
+
+  ngOnDestroy() {
+    this.disconnect();
+  }
 }
