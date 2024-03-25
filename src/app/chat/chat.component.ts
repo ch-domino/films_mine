906db662eb1ab4ee761db34a4d0497d3fd982c0e
ch.domino <chrobakdomino@gmail.com>
2024-03-25 12:01:39 +0100

    Add imports and methods to ChatComponent

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../../modules/material.module';
+import { ChatService } from '../../services/chat.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, FormsModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
 export class ChatComponent {
+  name = '';
+  chatService = inject(ChatService);
 
+  connect() {
+    this.chatService.connect().subscribe((ok) => {
+      console.log('Connected!');
+    });
+  }
+  disconnect() {}
 }
